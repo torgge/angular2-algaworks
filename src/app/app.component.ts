f022a546466f53ec3177ff61f975dfeefef46b58
George Bonespírito <torgge@gmail.com>
2017-12-24 18:55:30 -0200

    Binding de eventos customizados com @Output e EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,10 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ultimoID = 0;
-  nome = 'Teste';
-  adicionado = false;
   funcionarios = [];
 
-  adicionar() {
-    console.log(`Adicionado ${this.nome}`);
-    this.adicionado = true;
-
-    this.funcionarios.push({
-      id: Math.floor((Math.random() * 6) + 1),
-      nome: this.nome
-  });
+  aoAdicionar(funcionario) {
+    this.funcionarios.push(funcionario);
   }
+
 }
